7030ada608df79adae3a50dc87bf99faef42a5a4
Jochem Kuijpers <github.com@jochemkuijpers.nl>
2018-12-21 15:13:25 +0100

    Add vertex cover reduction by eliminating fully covered vertices

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vector>
 #include <random>
+#include <ctime>
+#include <algorithm>
 
 void load_graph(std::vector<std::pair<int, int>>* pGraph) {
     std::ifstream input("../graph");
@@ -22,28 +24,46 @@ std::vector<std::pair<int, int>> copy_graph(std::vector<std::pair<int, int>>* pG
     return newGraph;
 }
 
+void report(bool cover[1993], int* bestCover) {
+    int currentCover = 0;
+    for (int i = 0; i < 1993; i++) if (cover[i]) currentCover++;
+
+    if (currentCover < *bestCover) {
+        std::cout << "Found an improved cover:\n";
+        std::cout << "Previous: " << *bestCover << "\n";
+        std::cout << "Current:  " << currentCover << "\n";
+        for (int i =0; i < 1993; i++) {
+            std::cout << (cover[i] ? '1' : '0') << (i < 1992 ? "," : "\n\n");
+        }
+        *bestCover = currentCover;
+    }
+}
+
 int main() {
-    auto loaded_graph = std::vector<std::pair<int, int>>();
-    load_graph(&loaded_graph);
+    auto readonly_graph = std::vector<std::pair<int, int>>();
+    load_graph(&readonly_graph);
 
     int bestCover = 1993;
     bool cover[1993];
-    std::default_random_engine generator;
+    std::default_random_engine generator(static_cast<unsigned int>(std::time(0)));
 
+    long n = 0;
     while (true) {
-        for (int i = 0; i < 1993; i++) cover[i] = false;
-        auto graph = copy_graph(&loaded_graph);
+        n++;
+        if (n % 1000 == 0) { std::cout << n << " iterations... \n"; }
+        for (bool &i : cover) i = false;
+        auto graph = copy_graph(&readonly_graph);
 
         while (!graph.empty()) {
-            std::uniform_int_distribution<int> distribution(0, graph.size());
+            std::uniform_int_distribution<int> distribution(0, graph.size() - 1);
             int edge_idx = distribution(generator);
             int u = graph[edge_idx].first;
             int v = graph[edge_idx].second;
 
             auto it = graph.begin();
 
-            cover[u] = 1;
-            cover[v] = 1;
+            cover[u] = true;
+            cover[v] = true;
 
             while (it != graph.end()) {
                 if (it->first == u || it->first == v || it->second == u || it->second == v) {
@@ -54,19 +74,31 @@ int main() {
             }
         }
 
-        int currentCover = 0;
+        report(cover, &bestCover);
+
+        // we can remove a vertex from the cover if all of its neighbors are in the cover
+        // however, since we're approximating the vertex cover, we want to do this in a randomized order
+        int rnd_idx[1993];
+        for (int i = 0; i < 1993; i++) rnd_idx[i] = i;
+        std::shuffle(rnd_idx, rnd_idx + 1993, generator);
+
         for (int i = 0; i < 1993; i++) {
-            if (cover[i]) currentCover++;
-        }
-        if (currentCover < bestCover) {
-            std::cout << "Found an improved cover:\n";
-            std::cout << "Previous: " << bestCover << "\n";
-            std::cout << "Current:  " << currentCover << "\n";
-            for (int i =0; i < 1993; i++) {
-                std::cout << (cover[i] ? '1' : '0') << (i < 1992 ? "," : "\n\n");
+            int v = rnd_idx[i];
+            // assume all of it's edges are covered by it's neighbors, if we find a counter_example, set to false
+            bool fully_covered = true;
+
+            for (auto const &edge : readonly_graph) {
+                if (edge.first == v && !cover[edge.second] || edge.second == v && !cover[edge.first]) {
+                    fully_covered = false;
+                }
             }
-            bestCover = currentCover;
+
+            if (fully_covered) {
+                cover[v] = false;
+            }
         }
+
+        report(cover, &bestCover);
     }
 
     return 0;
